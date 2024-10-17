7e5d098cdaffdfd14ee2dee2e65766e420749f44
murtozayev <jahongirmurtozayev777@gmail.com>
2024-10-17 09:12:14 +0500

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = ({ setShow }) => {
 
     return (
         <nav>
-            <audio ref={audioRef} src="../../public/Interface_Sound_Effects__Futuristic_Computer_Sci_Fi_Sound_Effects.mp3" />
+            <audio itemType="audio/mp3" ref={audioRef} src="../../public/Interface_Sound_Effects__Futuristic_Computer_Sci_Fi_Sound_Effects.mp3" />
             {/* Blue Turn */}
 
 
